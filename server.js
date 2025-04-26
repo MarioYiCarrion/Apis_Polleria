0fc3f935f2e87b392739ac8f32e6cdf0622695a0
MarioYiCarrion <marioyicarrion@hotmail.com>
2025-04-25 20:07:20 -0500

    historial api1

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use('/api/salidas', require('./routes/salida.routes'));
 app.use('/api/stock', require('./routes/stock.routes'));
 app.use(express.json());
 app.use('/api', authRoutes);
-app.use('/api/historial', historialRoutes);
+app.use('/api/historial', historial.routes);
 
 
 const PORT = process.env.PORT || 3000;
