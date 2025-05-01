c3c547e174aad974262f5c385bedfae431e0fb95
MarioYiCarrion <marioyicarrion@hotmail.com>
2025-04-30 20:35:27 -0500

    arreglando bajostock1

--- routes/producto.routes.js
+++ routes/producto.routes.js
@@ -5,12 +5,13 @@ const controller = require('../controllers/producto.controller');
 
 
 router.get('/', controller.getAll);
+router.get('/stock-bajo', controller.getStockBajo);
 router.get('/:id', controller.getById);
 router.post('/', controller.create);
 router.put('/:id', controller.update);
 router.delete('/:id', controller.remove);
 router.get('/tipo/:tipoId', controller.getByTipo);
-router.get('/stock-bajo', controller.getStockBajo);
+
 
 
 module.exports = router;
