1304fa0da678220ffecf49193b956f8550f90a38
MarioYiCarrion <marioyicarrion@hotmail.com>
2025-06-23 19:39:22 -0500

    agregando reporte excel2

--- routes/stock.routes.js
+++ routes/stock.routes.js
@@ -3,10 +3,11 @@ const router = express.Router();
 const controller = require('../controllers/stock.controller');
 
 router.get('/', controller.getAll);
+router.get('/exportar-excel', controller.exportStockToExcel);
 router.get('/:id', controller.getById);
 router.post('/', controller.create);
 router.put('/:id', controller.update);
-router.delete('/:id', controller.remove);
-router.get('/exportar-excel', controller.exportStockToExcel)
+router.delete('/:id', controller.remove)
+
 
 module.exports = router;
